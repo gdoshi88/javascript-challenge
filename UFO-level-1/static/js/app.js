0d26fd34d7b381e719d1a98b8b902615f54b7f55
Gautam Doshi <gdoshi88@gmail.com>
2019-08-25 15:19:38 -0700

    initial_commit_2

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,8 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// take the data.js and put it in to a 'dataframe' type table(day 1 JavaScript)
+// columns for that table: `date/time`, `city`, `state`, `country`, `shape`, `comment`(Day 1 JavaScript)---> Convert to HTML table??
+// use the date form in the HTML and write JS code that will take iput for date/time and return the rows accordingly(Callbacks, Day 2 JavaScript)
+
+
